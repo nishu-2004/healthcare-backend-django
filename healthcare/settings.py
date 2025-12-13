816d2c08de7cf9dd20baf0dc338955eb5c07516b
nishu-2004 <nishuninad@gmail.com>
2025-12-13 11:31:43 +0530

    Initialize core apps and register them in settings

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Rest 
+    'rest_framework',
+
+    # My apps 
+    'accounts',
+    'patients',
+    'doctors',
+    'mappings',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +129,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
