ece9ae88ca1030cbee1d580205dff9442b46d386
nishu-2004 <nishuninad@gmail.com>
2025-12-14 10:54:58 +0530

    feat: add role-based appointment viewing

--- appointments/views.py
+++ appointments/views.py
@@ -9,5 +9,22 @@ class AppointmentViewSet(ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
 
+    # For dynamic viewing
+    def querysat(self):
+        queryset = super().get_queryset()
+        params = self.request.query_params
+
+        doctor = params.get("doctor")
+        patient = params.get("patient")
+        status = params.get("status")
+
+        if doctor:
+            queryset = queryset.filter(doctor_id = doctor)
+        if patient:
+            queryset = queryset.filter(doctor_id = doctor)
+        if status:
+            queryset = queryset.filter(doctor_id = doctor)
+        return queryset
+
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
